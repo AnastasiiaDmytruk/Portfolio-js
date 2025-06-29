df63255e332b00e9b39d0606b88121185a3a4ef9
AnastasiiaDmytruk <153641542+AnastasiiaDmytruk@users.noreply.github.com>
2025-06-29 20:46:33 +0200

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// import './js/about-me';
+// import './js/projects';
+// import './js/faq';
+// import './js/covers';
+// import './js/reviews';
+// import './js/menu';
+// import './js/work-together';
+// import './js/header';
+// import './js/scroll-btn';
